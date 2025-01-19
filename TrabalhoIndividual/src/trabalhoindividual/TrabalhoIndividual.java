1653f680170dd8cce261df98793668cd2e902a69
Yanmaiscedo <134351027+Yanmaiscedo@users.noreply.github.com>
2025-01-19 12:58:33 -0300

    feat: Criação da Janela Principal

--- TrabalhoIndividual/src/trabalhoindividual/TrabalhoIndividual.java
+++ TrabalhoIndividual/src/trabalhoindividual/TrabalhoIndividual.java
@@ -4,6 +4,8 @@
  */
 package trabalhoindividual;
 
+import trabalhoindividual.view.JanelaPrincipal;
+
 /**
  *
  * @author yanma
@@ -15,6 +17,10 @@ public class TrabalhoIndividual {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JanelaPrincipal janelaPrincipal = new JanelaPrincipal();
+        janelaPrincipal.setVisible(true);
+        
     }
     
 }
